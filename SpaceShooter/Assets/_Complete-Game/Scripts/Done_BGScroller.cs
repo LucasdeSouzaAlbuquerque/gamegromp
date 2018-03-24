39409cd0a3f4a11b43afef77dfd8bb75acbb586a
Lucas <lsa2@cin.ufpe.br>
2018-03-24 15:12:20 -0300

    Game

---
+++ SpaceShooter/Assets/_Complete-Game/Scripts/Done_BGScroller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_BGScroller : MonoBehaviour
+{
+	public float speed;
+    public float swing;
+    public bool up = false;
+
+	private Vector3 startPosition;
+    private Vector3 targetPosition;
+
+    void Start ()
+	{
+		startPosition = transform.position;
+        targetPosition = startPosition + (new Vector3(0,0,1) * swing);
+	}
+
+	void Update ()
+	{
+        Vector3 newPosition;
+        if(up == false)
+        {
+            newPosition = Vector3.MoveTowards(transform.position, targetPosition, speed);
+            transform.position = newPosition;
+            if(transform.position == targetPosition) { up = true; }
+        }
+        else {
+            newPosition = Vector3.MoveTowards(transform.position, startPosition, speed);
+            transform.position = newPosition;
+            if (transform.position == startPosition) { up = false; }
+        }
+	}
+}
---
+++ SpaceShooter/Assets/_Complete-Game/Scripts/Done_GameController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Done_GameController : MonoBehaviour
+{
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public float startWait;
+	public float laserWait;
+	
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+	
+	private bool gameOver;
+	private bool restart;
+	private int score;
+	
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnLasers ());
+	}
+	
+	void Update ()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			}
+		}
+	}
+	
+	IEnumerator SpawnLasers()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (hazard, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds (laserWait);
+			
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+	
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+	
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+}
